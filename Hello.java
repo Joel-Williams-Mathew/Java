f363ab01288bd4422670214a60662a6867ddb915
Joel Williams Mathew <joelw2200@gmail.com>
2024-09-26 20:44:18 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,11 @@
+//Author : Joel Williams
+//Date : 25/7/24
+//Aim : Write a program to print Hello World in java
+
 class Hello {
 
 	public static void main(String[] args) {
-		
+		//statement to print hello world
 		System.out.println("Hello world");
 }
 
