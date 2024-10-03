262a5565da596ac453958807456d5704e31f81dc
Joel Williams Mathew <joelw2200@gmail.com>
2024-10-03 11:20:50 +0530

    Add files via upload

---
+++ CheckInheritance.java
@@ -0,0 +1,20 @@
+class Animal {
+	public String name;
+	public void eat() {
+		System.out.println("Eat function in class Animal");
+	}
+}
+class Dog extends Animal {
+	public void display() {
+		System.out.println("New name is "+name);
+	}
+}
+
+class CheckInheritance {
+	public static void main(String[] args) {
+		Dog lab = new Dog();
+		lab.name = "Tiger";
+		lab.display();
+		lab.eat();
+	}
+}
